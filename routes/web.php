2acea75c47292e559ecf9826e782197ecb2f9c56
recca <flamerecca711@gmail.com>
2025-03-02 16:33:52 +0800

    feat:Add Index Testcase

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,36 @@
 <?php
 
+
 use Illuminate\Support\Facades\Route;
 use Livewire\Volt\Volt;
 
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
+
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use App\Models\User;
+Route::get('/users', function (Request $request) {
+    $name = $request->input('name');
+    $users = User::where('name', $name)->get();
+
+    return response()->json($users);
+});
+
+Route::get('/users-seed', function () {
+    User::factory()
+        ->count(100_000)
+        ->create();
+});
+
+Route::get('/users-with-index', function (Request $request) {
+    dd(User::where('email', $request->input('email'))->first());
+});
+
+Route::get('/users-with-index', function (Request $request) {
+    dd(User::where('email_without_index', $request->input('email'))->first());
+});
 
 Route::view('dashboard', 'dashboard')
     ->middleware(['auth', 'verified'])
