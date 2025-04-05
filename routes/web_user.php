03653324a455d8b87d5c12b013056d91422a248b
Recca Chao <flamerecca711@gmail.com>
2025-04-05 20:16:31 +0800

    Add web_user.php

---
+++ routes/web_user.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use Illuminate\Http\Request;
+
+Route::get('/users', function (Request $request) {
+    $name = $request->input('name');
+    $users = User::where('name', $name)->get();
+
+    return response()->json($users);
+});
+
+
+Route::get('/users-count', function () {
+    // 計算有幾位 user
+    return User::count();
+});
+
+Route::get('/users-with-index', function (Request $request) {
+    return response()->json(User::where('email', $request->input('email'))->get());
+});
+
+Route::get('/users-without-index', function (Request $request) {
+    return response()->json(User::where('email_without_index', $request->input('email'))->get());
+});
+
+Route::get('/users-with-index-explain', function (Request $request) {
+    return response()->json(User::where('email', $request->input('email'))->explain());
+});
+
+Route::get('/users-without-index-explain', function (Request $request) {
+    return response()->json(User::where('email_without_index', $request->input('email'))->explain());
+});
+
+Route::get('/users-rand', function (Request $request) {
+    return response()->json(User::inRandomOrder()->first());
+});
