7be8a1df5d1bcff08166ae73631124ca09488a51
Recca Chao <flamerecca711@gmail.com>
2025-12-27 08:02:28 +0800

    Add n8n Header Route

---
+++ app/Http/Controllers/N8nOrderHeaderController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Data\N8nOrder;
+use Illuminate\Http\Request;
+
+class N8nOrderHeaderController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $token = $request->header('unique_id');
+
+        // 檢查 Header 是否存在
+        if (!$request->hasHeader('unique_id')) {
+            return response()->json(['message' => '缺少 Header: unique_id']);
+        }
+        // 模擬資料（實務上你可能從外部 API 或記憶體取得）
+        return collect([
+            new N8nOrder(
+                orderID: 1,
+                customerID: 18,
+                employeeName: "Dave",
+                orderPrice: 253.32,
+                orderStatus: "processing"
+            ),
+            new N8nOrder(
+                orderID: 2,
+                customerID: 11,
+                employeeName: "Eve",
+                orderPrice: 209.19,
+                orderStatus: "booked"
+            ),
+            new N8nOrder(
+                orderID: 3,
+                customerID: 19,
+                employeeName: "Eve",
+                orderPrice: 211.19,
+                orderStatus: "booked"
+            ),
+            new N8nOrder(
+                orderID: 4,
+                customerID: 11,
+                employeeName: "Dave",
+                orderPrice: 201.71,
+                orderStatus: "booked"
+            ),
+            new N8nOrder(
+                orderID: 5,
+                customerID: 19,
+                employeeName: "Zoe",
+                orderPrice: 231.9,
+                orderStatus: "booked"
+            ),
+        ]);
+    }
+
+}
--- routes/api/n8n_order.php
+++ routes/api/n8n_order.php
@@ -1,5 +1,8 @@
 <?php
 
 use App\Http\Controllers\N8nOrderController;
+use App\Http\Controllers\N8nOrderHeaderController;
 
 Route::get('/n8n-order', N8nOrderController::class);
+
+Route::get('/n8n-order-header', N8nOrderHeaderController::class);
